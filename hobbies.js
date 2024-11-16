4fff4556339139af047e71eb274f58e009349953
VincenzoIuliano <bubucane@outlook.it>
2024-11-16 19:25:08 +0100

    ADD: second js file

---
+++ hobbies.js
@@ -0,0 +1,11 @@
+function hobbies (hobbyOne, hobbyTwo, hobbyThree) {
+    let myHobbies = { 
+        'hobbies' : [hobbyOne, hobbyTwo, hobbyThree]
+    }
+    console.log(myHobbies);
+    
+}
+
+hobbies('nuoto','calcio','pallavolo')
+
+module.export = hobbies
